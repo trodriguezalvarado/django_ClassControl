7ff0ed179b38a23fdbfe8d5f6325dd03fe90bda4
tomas <tomas.rodriguez@ntsprint.com>
2024-04-24 10:16:37 -0400

    Add the initial configurations necessary for the site to function.

--- University/University/urls.py
+++ University/University/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('ClassControls/', include('ClassControls.urls')),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/ClassControls/', permanent=True)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
